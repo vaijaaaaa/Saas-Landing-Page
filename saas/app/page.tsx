3c5f02f6a7027598b4adf4925bc51ea7f5ac7aa6
vaijaaaaa <vaijuwalker111@gmail.com>
2025-01-05 11:52:38 +0530

    shiny setup

--- saas/app/page.tsx
+++ saas/app/page.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { DotPattern } from "@/components/magicui/dot-pattern";
-import FlipText from "@/components/ui/flip-text";
 import WordRotate from "@/components/ui/word-rotate";
+import SparklesText from "@/components/ui/sparkles-text";
+import AnimatedShinyText from "@/components/ui/animated-shiny-text";
 
-const words = ["welcome", "To","Our Website"];
+const words = ["welcome", "To", "Our Website"];
+
+
 function Home() {
   return (
     <div className="h-screen flex items-center justify-center bg-gray-50">
       <DotPattern>
       </DotPattern>
-       <WordRotate words={words} className="text-6xl font-bold" />
-       
+        <div className="flex flex-col items-center">
+          <WordRotate words={words} className="text-6xl font-bold mb-4" />
+          <SparklesText text="This is best ai website" className="text-1xl mt-4" />
+     
+
+        </div>
+     
     </div>
   );
 }
